49121f21d82afcbce8f60b5fa865bebcebe4ab2a
sarfarajansari <com.sarfarajansari@gmail.com>
2021-08-01 12:48:28 +0530

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
 
-let showpaths=[]
+
 
 function animateTitle(words,id){
   let k=0
@@ -28,38 +28,35 @@ function animateTitle(words,id){
 
 
 document.addEventListener('DOMContentLoaded',()=>{
-  function Path(x, y){
-    var xpercent = window.innerWidth / 100;
-    var h = document.getElementsByClassName("paper-plane-animation")[0].offsetHeight
-    var ypercent =h/100
-  
-    return {
-      x: x * xpercent,
-      y: y *ypercent,
-      opacity: 1,
-    };
-  };
-  let factor = window.innerWidth<=725?-10:0
-  let paths = [
-    Path(90+factor, 20),
-    Path(90+factor,10),
-
-    Path(0 ,40),
-    Path(0 ,30),
-
-    Path(90 + factor, 60),
-    Path(90 + factor,50),
-
-    Path(0 ,80),
-    Path(0 ,70),
-
-    Path(70, 90),
-    Path(100,70)
-  ];
-  showpaths=paths;
-
-  
   function SingleScroll(){
+    function Path(x, y){
+      var xpercent = window.innerWidth / 100;
+      var h = document.getElementsByClassName("paper-plane-animation")[0].offsetHeight
+      var ypercent =h/100
+    
+      return {
+        x: x * xpercent,
+        y: y *ypercent,
+        opacity: 1,
+      };
+    };
+    let factor = window.innerWidth<=725?-10:0
+    let paths = [
+      Path(90+factor, 20),
+      Path(90+factor,10),
+  
+      Path(0 ,40),
+      Path(0 ,30),
+  
+      Path(90 + factor, 60),
+      Path(90 + factor,50),
+  
+      Path(0 ,80),
+      Path(0 ,70),
+  
+      Path(70, 90),
+      Path(100,70)
+    ];
     const flightPath = {
       path: paths,
       curviness: 1.25,
@@ -82,8 +79,9 @@ document.addEventListener('DOMContentLoaded',()=>{
     });
     console.log(window.innerHeight)
   };
-  SingleScroll()
-  animateTitle("MY LEARNING PATH","#path-title")
+  // SingleScroll()
+  // animateTitle("MY LEARNING PATH","#path-title")
+  animateTitle("HAPPY FRIENDSHIP DAY SARFARAJ K DOSTO","#path-title")
 
 
 
